99ff41c8ad0619da19890c3a49d22b47824bdece
DESKTOP-148HI6V\alirodhi <alirodhi123@gmail.com>
2018-05-22 22:18:21 +0700

    bismillah lancar

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
 	//url : 'mongodb://udb-broiler:udb-broiler@db-broiler-shard-00-00-xwl6l.mongodb.net:27017,db-broiler-shard-00-01-xwl6l.mongodb.net:27017,db-broiler-shard-00-02-xwl6l.mongodb.net:27017/test?ssl=true&replicaSet=db-broiler-shard-0&authSource=admin'
-	url: 'mongodb://ali:alialiali@localhost:64526/broilerschemas'
+	url: 'mongodb://ali:alialiali@localhost:64526/broiler'
 }
