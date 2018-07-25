9b6f9cb5e6a2ddcc46e3b73aed2ce10a9ab30942
DESKTOP-148HI6V\alirodhi <alirodhi123@gmail.com>
2018-07-25 22:17:04 +0700

    final yallah

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
 	//url : 'mongodb://udb-broiler:udb-broiler@db-broiler-shard-00-00-xwl6l.mongodb.net:27017,db-broiler-shard-00-01-xwl6l.mongodb.net:27017,db-broiler-shard-00-02-xwl6l.mongodb.net:27017/test?ssl=true&replicaSet=db-broiler-shard-0&authSource=admin'
-	url: 'mongodb://ali:alialiali@jagopesan.com:64526/broiler' //untuk konek ke jago pesan, tapi kodingannya di local
-	//url: 'mongodb://ali:alialiali@localhost:64526/broiler' //untuk konek ke jago pesan, kodingan di push di jago pesan
+	//url: 'mongodb://ali:alialiali@jagopesan.com:64526/broiler' //untuk konek ke jago pesan, tapi kodingannya di local
+	url: 'mongodb://ali:alialiali@localhost:64526/broiler' //untuk konek ke jago pesan, kodingan di push di jago pesan
 }
